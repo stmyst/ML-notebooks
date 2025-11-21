d14cc5b53eebf736d34514eb47cc4c8f61d9bffa
stepan.martyugin <stepan.martyugin@nedra.digital>
2025-11-21 15:05:51 +0500

    add deepseek

---
+++ deep_seek_ocr.py
@@ -0,0 +1,123 @@
+# Stepan Martiugin https://t.me/compmtx
+# requirements: pymupdf, openai, tqdm
+"""Simple DeepSeek OCR converter for pdf files"""
+
+import base64
+import io
+import time
+from asyncio import Semaphore
+
+import pymupdf
+from PIL import Image
+from openai import AsyncOpenAI
+from openai.types.chat import (
+    ChatCompletionUserMessageParam,
+    ChatCompletionContentPartTextParam,
+    ChatCompletionContentPartImageParam,
+)
+from tqdm.asyncio import tqdm
+
+
+async def process_pdf_file(
+    pdf_file: str,
+    client: AsyncOpenAI,
+    prompt: str,
+    semaphore_slots: int = 50,
+    dpi: int = 200
+) -> None:
+    print(f'received {pdf_file}')
+    images = _cut_file_to_images(file=pdf_file, dpi=dpi)
+
+    recognized_pages = await _ocr_images(
+        images=images, client=client, prompt=prompt, semaphore_slots=semaphore_slots,
+    )
+
+    _save_ocr_results(recognized_pages=recognized_pages, file=pdf_file)
+
+
+def _save_ocr_results(recognized_pages: list[str | None], file: str) -> None:
+
+    to_save = f'{file}.txt'
+    with open(to_save, 'w', encoding='utf-8') as f:
+        for page_idx, page in enumerate(recognized_pages, start=1):
+            if page is None:
+                print(f'page {page_idx} was not recognized')
+            else:
+                f.write(page)
+                f.write('\n\n')
+        print(f'saved to {to_save}')
+
+
+def _cut_file_to_images(file: str, dpi: int) -> list[str]:
+    images = []
+    with pymupdf.open(file) as pdf_doc:
+        for page in pdf_doc:
+            pix = page.get_pixmap(dpi=dpi)
+            image_rgb = Image.frombytes(mode='RGB', size=[pix.width, pix.height], data=pix.samples)
+            with io.BytesIO() as buffer:
+                image_rgb.save(buffer, format='PNG')
+                images.append(base64.b64encode(buffer.getvalue()).decode('utf-8'))
+        return images
+
+
+async def _ocr_images(
+    images: list[str], client: AsyncOpenAI, prompt: str, semaphore_slots: int
+) -> list[str]:
+    semaphore = Semaphore(semaphore_slots)
+    text_prompt = ChatCompletionContentPartTextParam(type='text', text=prompt)
+    tasks = [
+        _prompt_to_model(
+            semaphore=semaphore,
+            client=client,
+            content=[
+                text_prompt,
+                ChatCompletionContentPartImageParam(
+                    type='image_url', image_url={'url': f'data:image/png;base64,{image}'},
+                )
+            ],
+        ) for image in images
+    ]
+    return await tqdm.gather(*tasks, desc='Processing images')
+
+
+async def _prompt_to_model(
+    semaphore: Semaphore,
+    client: AsyncOpenAI,
+    content: list[ChatCompletionContentPartTextParam | ChatCompletionContentPartImageParam],
+) -> str | None:
+    try:
+        async with semaphore:
+            response = await client.with_options(max_retries=3).chat.completions.create(
+                model='deepseek-ai/DeepSeek-OCR',
+                messages=[ChatCompletionUserMessageParam(role='user', content=content)],
+                seed=0,
+                temperature=0,
+                extra_body={'ngram_size': 30, 'window_size': 90},
+            )
+            return response.choices[0].message.content
+    except Exception as error:
+        print(f'error {error}')
+        return None
+
+
+if __name__ == '__main__':
+    import asyncio
+
+    async def process_pdfs():
+
+        aclient = AsyncOpenAI(
+            base_url='url',
+            api_key='api_key',
+        )
+        path = r'path_to_folder'
+        files = ('file_1.pdf', 'file_2.pdf')
+
+        for file in files:
+            start_time = time.perf_counter()
+            await process_pdf_file(
+                pdf_file=f'{path}\\{file}', client=aclient, prompt='<image>\nFree OCR.'
+            )
+            elapsed = (time.perf_counter() - start_time) / 60
+            print(f'processed {file}, for {elapsed:.2f} min')
+
+    asyncio.run(process_pdfs())
